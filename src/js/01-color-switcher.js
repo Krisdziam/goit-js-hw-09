054dbc75de2a937e9a0f131e7267792356364ce0
Kristian <kris.dziamka@gmail.com>
2022-08-08 22:58:53 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
-console.log(" World");
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+
+  const body = document.querySelector('body')
+
+  console.log(body);
