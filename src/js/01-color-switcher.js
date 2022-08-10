830468692a126ee9ea61d25f20787590cd421df1
Kristian <kris.dziamka@gmail.com>
2022-08-10 20:07:21 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,29 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
+const startBtn = document.querySelector('[data-start]')
+const closeBtn = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
 
-  const body = document.querySelector('body')
+ 
+function colorSwitcher (){
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  
+  startBtn.disabled = true;
+  // startBtn.removeEventListener('click',colorSwitcher)
+}
 
-  console.log(body);
+function disableColorSwitcher (){
+ 
+  startBtn.disabled = false;
+clearInterval(intervalId);
+
+// startBtn.addEventListener('click', colorSwitcher);
+
+}
+startBtn.addEventListener('click', colorSwitcher);
+closeBtn.addEventListener('click', disableColorSwitcher);
+
+
